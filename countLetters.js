9b0c2d5dcbf089dc6d68001680b8a2c7a490ed86
zhanjerr <jerry.h.zhan@gmail.com>
2017-04-05 17:24:24 +0000

    solved the problem with two for loops

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+function countLetters(str){
+  var input = str.split(" ").join("").toLowerCase();
+  var distinctLetters = [];
+  var distinctLetterCount = {};
+  //find all distinct letter, assign to new array
+  // console.log(str.split(" ").join(""));
+  // console.log(distinctLetters.indexOf("l"));
+  for (var i in input){
+    if (distinctLetters.indexOf(input[i]) > -1){
+      // console.log("found it");
+    } else {
+      distinctLetters.push(input[i]);
+      // console.log("got to else");
+    }
+  }
+  // console.log(distinctLetters);
+  //loop through and count occurance of each letter in array
+  for (var i of distinctLetters){
+    distinctLetterCount[i] = input.split(i).length - 1;
+  }
+  console.log(distinctLetterCount);
+}
+
+// countLetters("light house");
+// countLetters("lighthouse in the house");
